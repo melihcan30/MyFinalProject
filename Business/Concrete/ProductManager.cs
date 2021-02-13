7d4fa619757a9c39522750cc29fc036fa2c825b0
melihcan serbest <mcan-serbest@hotmail.com>
2021-02-13 14:21:37 +0300

    a

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -15,11 +15,23 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
+
+
         public List<Product> GetAll()
         {
             //İş kodları
 
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
